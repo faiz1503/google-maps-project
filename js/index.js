3b32e4aed85b21ffacf14538c9f47acf2c7dd912
faiz1503 <faiz16ti@mahasiswa.pcr.ac.id>
2020-10-26 17:45:05 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var map;
+var markers = [];
+var infoWindow;
+var locationSelect;
+
+function initmap() {
+    var sydney = {
+        lat: -33.863276,
+        lng: 151.107977
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: sydney,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        }
+    });
+}
